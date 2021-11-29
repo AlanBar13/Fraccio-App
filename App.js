85358f8bc1d67b4e35d2fe70d918f6a7d905aba7
Alan Bardales <alan.g.bardales@gmail.com>
2021-11-29 15:45:58 -0600

    Added Native Base

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux'
+import { NativeBaseProvider } from 'native-base';
 
 import store from './store'
 
@@ -8,9 +9,11 @@ import Menu from './src/components/Menu';
 const App = () => {
   
   return (
-    <Provider store={store}>
-      <Menu />
-    </Provider>
+    <NativeBaseProvider>
+      <Provider store={store}>
+        <Menu />
+      </Provider>
+    </NativeBaseProvider>
   )
 }
 
