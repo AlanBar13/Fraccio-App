ac462db3d6c6ee1a3f3cb33a63f4b3892539dcd8
Alan Bardales <alan.g.bardales@gmail.com>
2021-11-30 15:18:34 -0600

    Login UI new design

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,49 +1,54 @@
 import React, {useState} from 'react'
-import { View, Text, StyleSheet, Button, TextInput, ActivityIndicator } from 'react-native'
+import {Box, FormControl, Input, Stack, WarningOutlineIcon, Button, Center, Heading, useToast} from 'native-base'
 import {useDispatch, useSelector} from 'react-redux'
 
 import { signIn } from '../actions/authActions'
 
 const Login = () => {
     const dispatch = useDispatch()
-    const {isLoading} = useSelector((state) => state.auth)
+    const {isLoading, error} = useSelector((state) => state.auth)
 
     const [user, setUser] = useState("")
     const [pass, setPass] = useState("")
 
+    const toast = useToast()
+
     const login = () => {
         dispatch(signIn(user, pass))
+        if(error){
+            toast.show({
+                title: "Usuario invalido o contraseña incorrecta",
+                status: "warning"
+            })
+        }
     }
+
     return (
-        <View style={styles.container}>
-           <Text style={styles.textStyle}>Login</Text>
-           <TextInput 
-                placeholder="Usuario"
-                value={user}
-                onChangeText={setUser}
-            />
-           <TextInput 
-                placeholder="Contraseña"
-                value={pass}
-                onChangeText={setPass}
-                secureTextEntry 
-            />
-           <Button title="login" onPress={() => login()}></Button>
-           {isLoading === false ? <></> : <ActivityIndicator />} 
-        </View>
+        <Center flex={1} px={3}>
+            <Heading textAlign="center" mb="10">Fraccio</Heading>
+            <Box w={{
+                base:"90%",
+                md: "25%"
+            }}
+            space={2}>
+                <FormControl>
+                    <Stack mx="4" paddingTop="2">
+                        <FormControl.Label>Usuario:</FormControl.Label>
+                        <Input value={user} onChangeText={setUser} />
+                    </Stack>
+                </FormControl>
+                <FormControl>
+                    <Stack mx="4" paddingTop="2">
+                        <FormControl.Label>Contraseña:</FormControl.Label>
+                        <Input type="password" value={pass} onChangeText={setPass} />
+                    </Stack>
+                </FormControl>
+                <Stack mx="4" paddingTop="2">
+                    <Button isLoading={isLoading} spinnerPlacement="end" onPress={() => login()}>Log In</Button>
+                </Stack>
+            </Box>
+        </Center>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    textStyle: {
-        fontSize: 16,
-        fontWeight: '700'
-    }
-})
 
 export default Login
